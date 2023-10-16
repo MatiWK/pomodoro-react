cbc35bd511b80d3101899dabb2514c6379557759
Mateusz Weltrowski - Knopik <raixerlgr@gmail.com>
2023-10-16 15:36:02 +0200

    handlers to component

--- src/components/TimerButtons.tsx
+++ src/components/TimerButtons.tsx
@@ -1,14 +1,58 @@
+import { useSetAtom } from "jotai";
 import { useTimer } from "../contexts/TimerContext";
 import { LONG_BREAK, POMODORO, SHORT_BREAK, modes } from "../contexts/modes";
+import { chosenTimerAtom } from "../atoms/chosen-timer-atom";
+import { timeAtom } from "../atoms/time-atom";
+import { isRunningAtom } from "../atoms/is-running-atom";
+import { useColorSwitch } from "../hooks/use-color-switch";
+import { colorLinks } from "./colorLinks";
 
 const inactiveButton = "mx-3";
 const activeButton =
   inactiveButton + " transparent-background px-2 py-1 rounded-lg";
 const labels = modes;
+const { pomodoroTimer, shortbreakTimer, longbreakTimer } = modes;
+
 
 const TimerButtons = () => {
-  const { chosenTimer, handlers } = useTimer();
+  const { chosenTimer } = useTimer();
+  const setChosenTimer = useSetAtom(chosenTimerAtom)
+  const setTime = useSetAtom(timeAtom);
+  const setIsRunning = useSetAtom(isRunningAtom);
+  const colorSwitch = useColorSwitch();
 
+
+
+
+  function handlePomodoro(e: any) {
+    e.preventDefault();
+    // choses link to apply styling to
+    setChosenTimer("pomodoroTimer");
+    // sets time according to the selected one
+    setTime(pomodoroTimer.initialTime);
+    setIsRunning(false);
+
+    // sends chosen link to app.jsx
+    colorSwitch(colorLinks.pomodoro);
+  }
+
+  function handleShortbreak(e: any) {
+    e.preventDefault();
+    setChosenTimer("shortbreakTimer");
+    setTime(shortbreakTimer.initialTime);
+    setIsRunning(false);
+
+    colorSwitch(colorLinks.break);
+  }
+
+  function handleLongBreak(e: any) {
+    e.preventDefault();
+    setChosenTimer("longbreakTimer");
+    setTime(longbreakTimer.initialTime);
+    setIsRunning(false);
+
+    colorSwitch(colorLinks.break);
+  }
   
 
 
@@ -17,21 +61,21 @@ const TimerButtons = () => {
       <a
         href={labels[POMODORO].label}
         className={chosenTimer === POMODORO ? activeButton : inactiveButton}
-        onClick={handlers.handlePomodoro}
+        onClick={handlePomodoro}
       >
         Pomodoro
       </a>
       <a
         href={labels[SHORT_BREAK].label}
         className={chosenTimer === SHORT_BREAK ? activeButton : inactiveButton}
-        onClick={handlers.handleShortbreak}
+        onClick={handleShortbreak}
       >
         Short Break
       </a>
       <a
         href={labels[LONG_BREAK].label}
         className={chosenTimer === LONG_BREAK ? activeButton : inactiveButton}
-        onClick={handlers.handleLongBreak}
+        onClick={handleLongBreak}
       >
         Long Break
       </a>
--- src/contexts/TimerContext.tsx
+++ src/contexts/TimerContext.tsx
@@ -5,10 +5,8 @@ import {
   useEffect,
 } from "react";
 import { modes } from "./modes";
-import { colorLinks } from "../components/colorLinks";
 import { useAtom, useSetAtom } from "jotai";
 import { isRunningAtom } from "../atoms/is-running-atom";
-import { runningAtom } from "../atoms/running-atom";
 import { timeAtom } from "../atoms/time-atom";
 import { chosenTimerAtom } from "../atoms/chosen-timer-atom";
 import { useColorSwitch } from "../hooks/use-color-switch";
@@ -18,11 +16,6 @@ type ContextValue = {
   backgroundColor: any;
   taskColor: any;
   isRunning: any;
-  handlers: {
-    handleLongBreak: (e: any) => void;
-    handlePomodoro: (e: any) => void;
-    handleShortbreak: (e: any) => void;
-  };
   restart: () => void;
   toggle: () => void;
 };
@@ -56,7 +49,6 @@ const TimerRunner = ({
   return null;
 };
 
-const { pomodoroTimer, shortbreakTimer, longbreakTimer } = modes;
 export const TimerContext = createContext<null | ContextValue>(null);
 export const TimerProvider = ({ children }: PropsWithChildren) => {
   const setTime = useSetAtom(timeAtom);
@@ -75,36 +67,6 @@ export const TimerProvider = ({ children }: PropsWithChildren) => {
     setIsRunning(false);
   };
 
-  // CHOOSING COUNTDOWN LOGIC
-  function handlePomodoro(e: any) {
-    e.preventDefault();
-    // choses link to apply styling to
-    setChosenTimer("pomodoroTimer");
-    // sets time according to the selected one
-    setTime(pomodoroTimer.initialTime);
-    setIsRunning(false);
-
-    // sends chosen link to app.jsx
-    colorSwitch(colorLinks.pomodoro);
-  }
-
-  function handleShortbreak(e: any) {
-    e.preventDefault();
-    setChosenTimer("shortbreakTimer");
-    setTime(shortbreakTimer.initialTime);
-    setIsRunning(false);
-
-    colorSwitch(colorLinks.break);
-  }
-
-  function handleLongBreak(e: any) {
-    e.preventDefault();
-    setChosenTimer("longbreakTimer");
-    setTime(longbreakTimer.initialTime);
-    setIsRunning(false);
-
-    colorSwitch(colorLinks.break);
-  }
 
   function toggle() {
     setIsRunning((prev: any) => !prev);
@@ -115,11 +77,6 @@ export const TimerProvider = ({ children }: PropsWithChildren) => {
     backgroundColor: modes[chosenTimer].backgroundClassName,
     taskColor: modes[chosenTimer].taskColorClassName,
     isRunning,
-    handlers: {
-      handleLongBreak,
-      handlePomodoro,
-      handleShortbreak,
-    },
     restart: takeNextTimer,
     toggle,
   };
